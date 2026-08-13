b3077b4895a7e9b555f87af855659cb598ff1fda
ALGABL511x <hsynalab99@gmail.com>
2026-08-13 17:25:40 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const path = require('path');
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+    socket.on('join-room', (userId) => {
+        socket.join(userId);
+    });
+
+    socket.on('signal', (data) => {
+        socket.to(data.target).emit('signal', { sender: data.sender, data: data.data });
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+    console.log('Server is running on port ' + PORT);
+});
